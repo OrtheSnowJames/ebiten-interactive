3621a982ecf7e3ea3a887d150d8a834bac5c445b
OrtheSnowJames <jamesyacoube@yahoo.com>
2025-03-23 09:02:11 -0700

    add new tag

---
+++ newver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <commit-message> <tag>"
+    exit 1
+fi
+
+COMMIT_MESSAGE=$1
+TAG=$2
+
+# Stage all changes
+git add .
+
+# Commit with the provided message
+git commit -m "$COMMIT_MESSAGE"
+
+# Create a new tag
+git tag "$TAG"
+
+# Push the commit and the tag
+git push
+git push origin "$TAG"
